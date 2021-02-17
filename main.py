cf319999f9d0b8c24b4309d50dca6476f689136a
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-02-17 13:46:58 +0900

    enable resizing

--- main.py
+++ main.py
@@ -13,26 +13,35 @@ def main():
         print('Not exist files in inputs.')
         return
 
-    size = input_size()
+    # size = input_size()
+    width = input_width()
+
+    format_to = 'png'
 
     for file in inputs:
         split_file_name = file.split(".")
         file_name = ''.join(split_file_name[:-1])
         extension = split_file_name[-1]
 
-        format_to = 'png'
-
-        convert_image(file_name, extension, format_to, size)
+        convert_image(file_name, extension, format_to, width)
 
 
-def input_size() -> tuple[int, int]:
+# 将来的にはこっちだけ使いたい
+# def input_size() -> tuple[int, int]:
+#     print('width: ', end='')
+#     width = int(input())
+#
+#     print('height: ', end='')
+#     height = int(input())
+#
+#     return width, height
+
+
+def input_width() -> int:
     print('width: ', end='')
     width = int(input())
 
-    print('height: ', end='')
-    height = int(input())
-
-    return width, height
+    return width
 
 
 # todo(kondo): svgも変換したい
@@ -40,20 +49,37 @@ def convert_image(
     file_name: str,
     extension: str,
     format_to: str,
-    size: tuple[int, int]
+    width: int = None,
+    height: int = None
 ):
     print(f'Start to convert {input_path}/{file_name}.{extension}.')
 
     try:
         img = Image.open(f'{input_path}/{file_name}.{extension}')
-        resized_img = img.resize(size)
+        img_width, img_height = img.size
+
+        if width is not None and height is not None:
+            resized_img = img.resize((width, height))
+
+        elif width is not None:
+            height = round(img_height * (width / img_width))
+            resized_img = img.resize((width, height))
+
+        elif height is not None:
+            width = round(img_width * (height / img_height))
+            resized_img = img.resize((width, height))
+
+        else:
+            raise ValueError('Both width and height are None.')
+
         resized_img.save(f'{output_path}/{file_name}.{format_to}')
     except UnidentifiedImageError as e:
         print(e)
         return
 
     print(
-        f'Finish to convert and saved {output_path}/{file_name}.{format_to}.')
+        f'Finish to convert and saved {output_path}/{file_name}.{format_to}.'
+    )
 
 
 if __name__ == '__main__':
